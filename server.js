105dbbd6d73fe142bca4be27fd9f2f855275d304
rquiroz <rquiroz3290@gmail.com>
2021-03-19 16:47:39 -0400

    have a server that runs, and added start scripts to concurrently start server and client

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const path = require("path")
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({extended: true}));
+
+
+
+
+if (process.env.NODE_ENV==="production"){
+    app.use(express.static("client/build"));
+}
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname,"../client/build/index.html"))
+});
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log("Listening on Port" + PORT)
+})
