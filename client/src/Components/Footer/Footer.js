f8eb35853db048b259290cca7478b37d916d5e1a
rquiroz3290 <rquiroz3290@gmail.com>
2021-04-02 09:09:57 -0400

    updated media query for mobile about section and some other footer/hero changes

--- client/src/Components/Footer/Footer.js
+++ client/src/Components/Footer/Footer.js
@@ -6,7 +6,15 @@ class Footer extends Component {
     render() {
         return (
             <section className='section-Footer'>
-
+                <div className='contact-info'>
+                    <p>Richard Quiroz</p>
+                    <p>Email: rquiroz3290@gmail.com</p>
+                    <p>Phone: 7324230560</p>
+                </div>
+                <div className='socials'>
+                    <h3>Socials</h3>
+                    
+                </div>
             </section>
         )
     }
