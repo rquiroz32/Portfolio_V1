27736764f6314788460c10d69f116279eb629bb1
rquiroz <rquiroz3290@gmail.com>
2021-04-13 15:01:29 -0400

    added inputs labels and button for contact form, with some initial styling, also removed unnecessary background color on project cards

--- client/src/Components/Contact/Contact.js
+++ client/src/Components/Contact/Contact.js
@@ -7,6 +7,23 @@ class Contact extends Component {
         return(
             <section className='section-Contact'>
                 <h1 className='contactTitle'>Contact</h1>
+                
+                    <form className ='form-container'>
+                        <label for='form-Name'>Name</label>
+                        <br></br>
+                        <input placeholder='Name' className='form-Name'></input>
+                        <br></br>
+                        <label for ='form-Subject'>Subject</label>
+                        <br></br>
+                        <input placeholder='Subject' className='form-Subject'></input>
+                        <br></br>
+                        <label for='form-Message'> Message</label>
+                        <br></br>
+                        <input placeholder ='Message'className='form-Message'></input>
+                        <br></br>
+                        <button className='form-Submit' /*type='submit'*/>Submit</button>
+                    </form>
+                
             </section>
         )
     }
