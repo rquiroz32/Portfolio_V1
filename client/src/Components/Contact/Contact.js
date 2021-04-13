c0cfb4a5c4bc7393175c76a9343bbd76cf774361
rquiroz <rquiroz3290@gmail.com>
2021-04-13 15:18:37 -0400

    fixed bug caused by case sensitive className to apply styles to form objects

--- client/src/Components/Contact/Contact.js
+++ client/src/Components/Contact/Contact.js
@@ -8,17 +8,17 @@ class Contact extends Component {
             <section className='section-Contact'>
                 <h1 className='contactTitle'>Contact</h1>
                 
-                    <form className ='form-container'>
+                    <form className ='form-Container'>
                         <label for='form-Name'>Name</label>
-                        <br></br>
+                        
                         <input placeholder='Name' className='form-Name'></input>
-                        <br></br>
+                        
                         <label for ='form-Subject'>Subject</label>
-                        <br></br>
+                        
                         <input placeholder='Subject' className='form-Subject'></input>
-                        <br></br>
+                        
                         <label for='form-Message'> Message</label>
-                        <br></br>
+                        
                         <input placeholder ='Message'className='form-Message'></input>
                         <br></br>
                         <button className='form-Submit' /*type='submit'*/>Submit</button>
