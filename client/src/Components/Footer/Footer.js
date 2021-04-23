8cf0829f27424734ebf250d9d9ecd46504f3215d
rquiroz3290 <rquiroz3290@gmail.com>
2021-04-23 19:20:38 -0400

    pushing updates to build

--- client/src/Components/Footer/Footer.js
+++ client/src/Components/Footer/Footer.js
@@ -1,14 +1,19 @@
 import { Component } from "react"
 import "./Footer.css"
-import gmail from "../../images/Socials/Gmail/iconmonstr-gmail-3-48.png"
-import github from '../../images/Socials/GitHub/iconmonstr-github-3-48.png'
-import linkedin from '../../images/Socials/LinkedIn/iconmonstr-linkedin-3-48.png'
+import gmail from "../../images/iconmonstr-gmail-3-48.png"
+import github from '../../images/iconmonstr-github-3-48.png'
+import linkedin from '../../images/iconmonstr-linkedin-3-48.png'
 
 
 
 class Footer extends Component {
 
+    
+    
     render() {
+
+        // const personalEmail = process.env.REACT_APP_PERSONAL_EMAIL
+        const {REACT_APP_PERSONAL_EMAIL} = process.env;
 
         const handleLinkedInOnClick = ()=>{
             window.location.href='https://www.linkedin.com/in/richard-quiroz-csm-a9880155/'
@@ -16,6 +21,11 @@ class Footer extends Component {
 
         const handleGitHubOnClick=()=>{
             window.location.href='https://github.com/rquiroz32'
+        }
+
+        const handleGmail=()=>{
+            console.log(`the email is ${REACT_APP_PERSONAL_EMAIL}`)
+            // window.location.href=`mailto:${process.env.REACT_APP_PERSONAL_EMAIL}`
         }
 
         return (
@@ -26,9 +36,9 @@ class Footer extends Component {
                     <p>Phone: 7324230560</p>
                 </div> */}
                 <div className='socials'>
-                    <img src={gmail} ></img>
-                    <img src={github}></img>
-                    <img src={linkedin} onClick={handleLinkedInOnClick}></img>
+                    <img src={gmail} onClick={handleGmail} alt='gmail icon'></img>
+                    <img src={github} onClick={handleGitHubOnClick} alt = 'github icon'></img>
+                    <img src={linkedin} onClick={handleLinkedInOnClick} alt ='linkedin icon'></img>
                 </div>
             </section>
         )
