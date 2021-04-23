38f2838671b29f16e4cb26c70f6a6a490ae8eae7
rquiroz3290 <rquiroz3290@gmail.com>
2021-04-23 18:08:36 -0400

    removing height properties on sections so that height is dynamic and doesn't cause overflow issues

--- client/src/Components/Hero/Hero.js
+++ client/src/Components/Hero/Hero.js
@@ -6,21 +6,25 @@ class Hero extends Component {
 
     render() {
 
-        const handlePortfolioBtn = ()=>{ window.location.href='/#portfolio'}
-        const handleContactBtn = ()=>{ window.location.href='/#contact'}
+        const handlePortfolioBtn = () => { window.location.href = '/#portfolio' }
+        const handleContactBtn = () => { window.location.href = '/#contact' }
 
         return (
             <header className="Hero">
                 <img className="HeroImg" src={image}></img>
 
                 <div className="contentContainer">
-                    <h1 className="HeroTxt">Hi I'm Richard, <br /> I'm a Full Stack Web Developer. </h1>
-
+                    <div className='universal-Test'>
+                        <h1 className="HeroTxt">Hi I'm Richard, <br /> I'm a Full Stack Web Developer. </h1>
+                    </div>
+                    
                     <div className="BtnContainer">
                         <button className="HeroBtn" onClick={handlePortfolioBtn} >See My Work</button>
                         <button className="HeroBtn" onClick={handleContactBtn} >Get in Touch</button>
                     </div>
                 </div>
+
+
             </header>
         )
     }
--- server.js
+++ server.js
@@ -5,15 +5,14 @@ const PORT = process.env.PORT || 3001;
 const dotenv = require('dotenv');
 const nodemailer = require('nodemailer')
 
-//express middleware
-// app.use(express.urlencoded({ extended: true }));
+
 app.use(express.json());
 app.use(function(req,res,next){
     res.header("Access-Control-Allow-Origin", "*")
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
     next()
 })
-// app.use(express.urlencoded({ extended: true}), express.json());
+
 
 // add env variables from .env
 dotenv.config()
