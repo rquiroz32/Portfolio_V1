b0d68126a4a208612c70ec4d62f659d127205c32
rquiroz3290 <rquiroz3290@gmail.com>
2021-07-06 17:11:19 -0400

    hamburger onClick test

--- client/src/Components/Navbar/Navbar.js
+++ client/src/Components/Navbar/Navbar.js
@@ -1,36 +1,45 @@
-import {Component} from "react"
+import { Component } from "react"
 import "./Navbar.css"
 
 
-class Navbar extends Component{
 
-    render(){
-        return(
-        <nav className="nav">
-            
-            <ul className ="nav-links">
-                <li>
-                    <a href="/">Home</a>
-                </li>
-                <li>
-                    <a href="#about">About</a>
-                </li>
-                <li>
-                    <a href="#portfolio">Portfolio</a>
-                </li>
-                <li>
-                    <a href="#contact">Contact</a>
-                </li>
-            </ul>
-            <div className="burger">
-                <div className = "line1"></div>
-                <div className = "line2"></div>
-                <div className = "line3"></div>
-            </div>
+class Navbar extends Component {
+
+   handleClick = ()=>{
+    const nav = document.querySelector('.nav-links');
+    nav.classList.toggle('nav-active');
+   }
+    
+
+    render() {
         
 
+        return (
+            <nav className="nav">
 
-        </nav>
+                <ul className="nav-links">
+                    <li>
+                        <a href="/">Home</a>
+                    </li>
+                    <li>
+                        <a href="#about">About</a>
+                    </li>
+                    <li>
+                        <a href="#portfolio">Portfolio</a>
+                    </li>
+                    <li>
+                        <a href="#contact">Contact</a>
+                    </li>
+                </ul>
+                <div className="burger" onClick={this.handleClick}>
+                    <div className="line1"></div>
+                    <div className="line2"></div>
+                    <div className="line3"></div>
+                </div>
+
+
+
+            </nav>
         )
     }
 
