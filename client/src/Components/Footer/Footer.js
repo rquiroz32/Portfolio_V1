243d6f7ab04222927f22d1702b9b9d55274a3899
rquiroz <rquiroz3290@gmail.com>
2021-07-20 13:35:31 -0400

    Removed 2 project placeholders, added info for 3rd project, fixed broken mailto icon

--- client/src/Components/Footer/Footer.js
+++ client/src/Components/Footer/Footer.js
@@ -24,17 +24,13 @@ class Footer extends Component {
         }
 
         const handleGmail=()=>{
-            console.log(`the email is ${REACT_APP_PERSONAL_EMAIL}`)
-            // window.location.href=`mailto:${process.env.REACT_APP_PERSONAL_EMAIL}`
+            //console.log(`the email is ${REACT_APP_PERSONAL_EMAIL}`)
+             window.location.href=`mailto:${process.env.REACT_APP_PERSONAL_EMAIL}`
         }
 
         return (
             <section className='section-Footer'>
-                {/* <div className='contact-info'>
-                    <p>Richard Quiroz</p>
-                    <p>Email: rquiroz3290@gmail.com</p>
-                    <p>Phone: 7324230560</p>
-                </div> */}
+                
                 <div className='socials'>
                     <img src={gmail} onClick={handleGmail} alt='gmail icon'></img>
                     <img src={github} onClick={handleGitHubOnClick} alt = 'github icon'></img>
