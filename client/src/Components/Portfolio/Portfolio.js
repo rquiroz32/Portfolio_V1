4fe963579f3f510aca62feea3c377f46de7eacfc
rquiroz3290 <rquiroz3290@gmail.com>
2021-07-20 16:52:34 -0400

    added final project need to make some styling tweaks still

--- client/src/Components/Portfolio/Portfolio.js
+++ client/src/Components/Portfolio/Portfolio.js
@@ -4,6 +4,7 @@ import "./Portfolio.css"
 import gBooks_img from '../../images/Gbooks.png'
 import employeeRegistry_img from '../../images/EmployeeRegistry.png'
 import dash_Img from '../../images/Dash.png'
+import porro_Img from '../../images/WeatherDashboard.png'
 
 class Portfolio extends Component {
 
@@ -18,7 +19,7 @@ class Portfolio extends Component {
                     <ProjectCard projTitle={'gbooks'} projImage={gBooks_img} projDescription={'This is a full MERN stack application for seraching books using the google books API and storing them in a MongoDB.'} projRepo={'https://github.com/rquiroz32/gbooks'} projDemo={'https://cryptic-caverns-34586.herokuapp.com/'}></ProjectCard>
                     <ProjectCard projTitle={'Employee Registry'} projImage={employeeRegistry_img} projDescription={'A React App that populates a table with random users from an API, and allows for searching and sorting by every field.'}projRepo={'https://github.com/rquiroz32/Employee_Registry'} projDemo={'https://rquiroz32.github.io/Employee_Registry/'} ></ProjectCard>
                     <ProjectCard projTitle={'Dash'} projImage={dash_Img} projDescription={' An application that displays the current day and contextually styles the elements on the page according to the time'}projRepo={'https://github.com/rquiroz32/Dash'} projDemo={'https://rquiroz32.github.io/Dash/'}></ProjectCard>
-                    <ProjectCard projTitle={'Test'} projImage={'https://via.placeholder.com/300'} projDescription={'This is a site that allows users to search for a covid testing location near them by zip code.'}projRepo={'https://github.com/rquiroz32/gbooks'} projDemo={'https://cryptic-caverns-34586.herokuapp.com/'}></ProjectCard>                    
+                    <ProjectCard projTitle={'Porro'} projImage={porro_Img} projDescription={'This application displays five day weather forcasts for cities searched by users.'}projRepo={'https://github.com/rquiroz32/Porro'} projDemo={'https://rquiroz32.github.io/Porro/'}></ProjectCard>                    
                 </div>
             </section>
         )
